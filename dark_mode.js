8f34ec0fb50e7cf1d5aff7357f186a6425a843b8
Timothy Langer <zeevox.dev@gmail.com>
2019-06-29 11:49:11 +0100

    Format HTML code
    * Used Google Chome DevTool's pretty print feature
    
    Signed-off-by: Timothy Langer <zeevox.dev@gmail.com>

--- dark_mode.js
+++ dark_mode.js
@@ -1,26 +1,27 @@
 const dark = "dark-mode";
 const light = "light-mode";
-var dark_on = true; // put the opposite of the default value here, i.e. if you want the page in light theme by default, we put true
+// put the opposite of the default value here, i.e. if you want the page in light theme by default, we put true
+var dark_on = true;
 
 function toggleDarkLight() {
-  dark_on = !dark_on;
-  var newClass = dark_on ? dark : light;
+    dark_on = !dark_on;
+    var newClass = dark_on ? dark : light;
 
-  var all = document.getElementsByTagName("*");
-  for (var i=0, max=all.length; i < max; i++) {
+    var all = document.getElementsByTagName("*");
+    for (var i = 0, max = all.length; i < max; i++) {
 
-    var classList = all[i].classList;
+        var classList = all[i].classList;
 
-    if (classList.contains(dark) || classList.contains(light))  {
-      classList.remove(dark);
-      classList.remove(light);
-      classList.add(newClass);
+        if (classList.contains(dark) || classList.contains(light)) {
+            classList.remove(dark);
+            classList.remove(light);
+            classList.add(newClass);
+        }
+
+        if (classList.length == 0) {
+            all[i].className = newClass;
+        }
     }
-
-    if (classList.length == 0) {
-      all[i].className = newClass;
-    }
-  }
 }
 
 toggleDarkLight();
